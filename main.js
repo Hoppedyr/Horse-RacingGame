cb50ec16662d4511e408892b64d4c1d1ae9ee1d5
Rasmus Jarnborg Friis <rasmusjf@hotmail.com>
2020-05-24 23:21:53 +0200

    added more logic to the game

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import * as cards from './cards.json';
 
 spadesCounter = 1;
 diamondsCounter = 1;
@@ -15,14 +14,253 @@ eight = false;
 nine = false;
 ten = false;
 
+moveBack = [];
+pickedCard = [];
+counter = 0
+
+
+shuffleArray = [];
+cardArray = [            
+{
+    "suit": "hearts",
+    "value": 2
+},
+{
+    "suit": "hearts",
+    "value": 3
+},
+{
+    "suit": "hearts",
+    "value": 4
+},
+{
+    "suit": "hearts",
+    "value": 5
+},
+{
+    "suit": "hearts",
+    "value": 6
+},
+{
+    "suit": "hearts",
+    "value": 7
+},
+{
+    "suit": "hearts",
+    "value": 8
+},
+{
+    "suit": "hearts",
+    "value": 9
+},
+{
+    "suit": "hearts",
+    "value": 10
+},
+{
+    "suit": "hearts",
+    "value": "J"
+},
+{
+    "suit": "hearts",
+    "value": "Q"
+},
+{
+    "suit": "hearts",
+    "value": "K"
+},
+{
+    "suit": "hearts",
+    "value": "A"
+},
+{
+    "suit": "diamonds",
+    "value": 2
+},
+{
+    "suit": "diamonds",
+    "value": 3
+},
+{
+    "suit": "diamonds",
+    "value": 4
+},
+{
+    "suit": "diamonds",
+    "value": 5
+},
+{
+    "suit": "diamonds",
+    "value": 6
+},
+{
+    "suit": "diamonds",
+    "value": 7
+},
+{
+    "suit": "diamonds",
+    "value": 8
+},
+{
+    "suit": "diamonds",
+    "value": 9
+},
+{
+    "suit": "diamonds",
+    "value": 10
+},
+{
+    "suit": "diamonds",
+    "value": "J"
+},
+{
+    "suit": "diamonds",
+    "value": "Q"
+},
+{
+    "suit": "diamonds",
+    "value": "K"
+},
+{
+    "suit": "diamonds",
+    "value": "A"
+},
+{
+    "suit": "clubs",
+    "value": 2
+},
+{
+    "suit": "clubs",
+    "value": 3
+},
+{
+    "suit": "clubs",
+    "value": 4
+},
+{
+    "suit": "clubs",
+    "value": 5
+},
+{
+    "suit": "clubs",
+    "value": 6
+},
+{
+    "suit": "clubs",
+    "value": 7
+},
+{
+    "suit": "clubs",
+    "value": 8
+},
+{
+    "suit": "clubs",
+    "value": 9
+},
+{
+    "suit": "clubs",
+    "value": 10
+},
+{
+    "suit": "clubs",
+    "value": "J"
+},
+{
+    "suit": "clubs",
+    "value": "Q"
+},
+{
+    "suit": "clubs",
+    "value": "K"
+},
+{
+    "suit": "clubs",
+    "value": "A"
+},
+{
+    "suit": "spades",
+    "value": 2
+},
+{
+    "suit": "spades",
+    "value": 3
+},
+{
+    "suit": "spades",
+    "value": 4
+},
+{
+    "suit": "spades",
+    "value": 5
+},
+{
+    "suit": "spades",
+    "value": 6
+},
+{
+    "suit": "spades",
+    "value": 7
+},
+{
+    "suit": "spades",
+    "value": 8
+},
+{
+    "suit": "spades",
+    "value": 9
+},
+{
+    "suit": "spades",
+    "value": 10
+},
+{
+    "suit": "spades",
+    "value": "J"
+},
+{
+    "suit": "spades",
+    "value": "Q"
+},
+{
+    "suit": "spades",
+    "value": "K"
+},
+{
+    "suit": "spades",
+    "value": "A"
+}
+]
+
+
+
+cardArray.forEach(element => {
+    pickedCard.push(false);
+});
+
+for (let i = 0; i < 52; i++) {
+    let randomCard = Math.floor(Math.random() * 51)  
+    if(pickedCard[randomCard] == false){
+        pickedCard[randomCard] == true;
+        shuffleArray[i] = cardArray[randomCard]
+    } else {
+        i--
+    }
+}
+
+for(let i = 0; i < 10; i++){
+    moveBack.push(shuffleArray[i])
+    counter++
+}
+
+console.log(shuffleArray);
+console.log(moveBack);
+
 
 
 
 $("#start").click(function () {
-    cardsShuffle = shuffle(card);
-    console.log(cardsShuffle);
-
     var inteval = setInterval(() => {
+        console.log(moveBack)
         if (game() != 'gameOver') {
             checkMoveBack()
         } else {
@@ -34,10 +272,19 @@ $("#start").click(function () {
 
 
 function game() {
-    let card = Math.floor(Math.random() * 4);
+
+    let randomNum = Math.floor(Math.random()* 51)
+    cardArray[randomNum]
+
+    if(cardArray[randomNum] != null){
+    console.log(cardArray[randomNum].suit)
+    
+    shuffleArray[counter]
+
+
     //$('#text').remove()
-    switch (card) {
-        case 0:
+    switch (cardArray[randomNum].suit) {
+        case 'spades':
             var cardToMove = 'spades'
             $(`#${cardToMove}` + spadesCounter).css('opacity', 0)
             spadesCounter++
@@ -48,7 +295,7 @@ function game() {
             }
             $('#card_move_spades').append("<p id='text'>spades moves</p>");
             break;
-        case 1:
+        case 'diamonds':
             var cardToMove = 'diamonds'
             $(`#${cardToMove}` + diamondsCounter).css('opacity', 0)
             diamondsCounter++
@@ -59,7 +306,7 @@ function game() {
             }
             $('#card_move_diamonds').append("<p id='text'>diamonds moves</p>");
             break;
-        case 2:
+        case 'hearts':
             var cardToMove = 'hearts'
             $(`#${cardToMove}` + heartsCounter).css('opacity', 0)
             heartsCounter++
@@ -70,7 +317,7 @@ function game() {
             }
             $('#card_move_hearts').append("<p id='text'>hearts moves</p>");
             break;
-        case 3:
+        case 'clubs':
             var cardToMove = 'clubs'
             $(`#${cardToMove}` + clubsCounter).css('opacity', 0)
             clubsCounter++
@@ -82,6 +329,9 @@ function game() {
             $('#card_move_clubs').append("<p id='text'>clubs moves</p>");
             break;
     }
+}else{
+    cardArray[randomNum] == null;
+}
 }
 
 
@@ -190,6 +440,8 @@ function reset() {
 
     $('.show_moved_back').remove()
 }
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-}
+
+
+
+
+    
